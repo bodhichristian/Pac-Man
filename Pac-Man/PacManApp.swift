2dfab2d3e176d547232bc421f519bc05ac2e3b78
bodhichristian <clavelle23@gmail.com>
2023-09-29 21:47:16 -0400

    initial commit
    really doing it.

---
+++ Pac-Man/PacManApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacManApp.swift
+//  PacMan
+//
+//  Created by christian on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PacManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameView()
+        }
+    }
+}
