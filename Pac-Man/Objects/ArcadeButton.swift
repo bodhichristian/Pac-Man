5284679f82ad6e82c2bdb32e2208c977d8bec2c2
bodhichristian <clavelle23@gmail.com>
2023-10-04 14:03:39 -0400

    Continue building ArcadeButton.

--- Pac-Man/Objects/ArcadeButton.swift
+++ Pac-Man/Objects/ArcadeButton.swift
@@ -8,21 +8,55 @@
 import SwiftUI
 
 struct ArcadeButton: View {
+    @State private var pressed = false
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
+                // Outer ring
                 Circle()
                     .stroke(lineWidth: geo.size.width * 0.1)
-                Circle()
-                    .padding(geo.size.width * 0.08)
+                
+                // Button
+ 
+                Button {
                     
-                Circle()
-                    .padding(geo.size.width * 0.08)
-                    .foregroundStyle(LinearGradient(colors: [.black, .clear, .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
+                } label : {
+                    ZStack {
+                        Circle()
+                            .padding(geo.size.width * 0.08)
+                        
+                        // Shading
+                        Circle()
+                            .padding(geo.size.width * 0.08)
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [.black, .clear, .clear],
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing
+                                )
+                            )
+                        Circle()
+                            .fill(
+                                RadialGradient(
+                                    gradient: Gradient(colors: [.white, .black]),
+                                    center: .center,
+                                    startRadius: 0,
+                                    endRadius: geo.size.width * 0.6
+                                )
+                            )
+                            .padding(geo.size.width * 0.15)
+                            .opacity(0.15)
+                    }
+                }
+                .withArcadeButtonStyle()
+                .onTapGesture {
+                    pressed = true
+                }
 
             }
             .foregroundStyle(.red)
-            .padding()
+            .padding(40)
         }
         
     }
@@ -31,3 +65,29 @@ struct ArcadeButton: View {
 #Preview {
     ArcadeButton()
 }
+
+
+
+struct ArcadeButtonStyle: ButtonStyle {
+    
+    let scaleAmount: CGFloat = 0.95
+    
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? scaleAmount : 1.0)
+            .brightness(configuration.isPressed ? -0.05 : 0)
+            
+    }
+}
+
+
+extension View {
+
+    func withArcadeButtonStyle() -> some View {
+        // No need to notate `self` - system will handle
+        buttonStyle(ArcadeButtonStyle())
+    }
+}
+
+
